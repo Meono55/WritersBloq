594664fbac98ad209c4b25276c3d7cd08156c48f
Anthony David Macpherson <dmacpherson864@gmail.com>
2019-02-28 16:32:01 -0500

    Created data transfer object for login credentials.

--- writers_bloq/src/main/java/com/revature/controllers/UserController.java
+++ writers_bloq/src/main/java/com/revature/controllers/UserController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.dto.CredentialsDTO;
 import com.revature.models.User;
 
-@CrossOrigin(methods= {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(methods = { RequestMethod.GET, RequestMethod.POST })
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -20,13 +20,13 @@ public class UserController {
 	/**
 	 * Retrieve a user from the database based off the provided login credentials.
 	 * 
-	 * @param email    The email of the user account to log into.
-	 * @param password The password of the user account to log into.
+	 * @param loginCredentials The data transfer object containing the login
+	 *                         information.
 	 * @return The user account associated with the provided credentials. Null is
 	 *         returned if no account could be found.
 	 */
-	@GetMapping(path = "/login", produces="application/json")
-	public User login(@RequestParam String email, @RequestParam String password) {
+	@PostMapping(path = "/login", produces = "application/json")
+	public User login(CredentialsDTO loginCredentials) {
 		return null;
 	}
 
@@ -45,12 +45,12 @@ public class UserController {
 	/**
 	 * Retrieves a user from the database.
 	 * 
-	 * @param userId The id of the user to retrieve from the database.
+	 * @param idValue The id of the user to retrieve from the database.
 	 * @return The user associated with the provided id number. Null is returned if
 	 *         the user could not be found or not be retrieved in the database.
 	 */
-	@GetMapping(path = "/{idValue}", produces="application/json")
-	public User getUser(@PathVariable(name="idValue") int userId) {
+	@GetMapping(path = "/{userId}", produces = "application/json")
+	public User getUser(@PathVariable(name = "userId") int idValue) {
 		return null;
 	}
 }
